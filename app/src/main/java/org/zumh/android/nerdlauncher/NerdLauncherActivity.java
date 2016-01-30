7c01b4319f95fb1fe4e2c89b4c40d38aa3280efd
tizz98 <dev.tizz98@gmail.com>
2016-01-30 15:09:49 -0500

    initial commit

---
+++ app/src/main/java/org/zumh/android/nerdlauncher/NerdLauncherActivity.java
@@ -0,0 +1,11 @@
+package org.zumh.android.nerdlauncher;
+
+import android.support.v4.app.Fragment;
+
+public class NerdLauncherActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return NerdLauncherFragment.newInstance();
+    }
+}
